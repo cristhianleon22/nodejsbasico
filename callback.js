f581c1fcef7a4267f75192129d614e11a1d8c7b8
cristhianleon22 <cristhiancleon@gmail.com>
2022-06-16 21:01:12 -0500

    callback

---
+++ callback.js
@@ -0,0 +1,10 @@
+function soyAsincrona(){
+    //console.log('hola, soy una funcion asincrona');
+    setTimeout(function(){
+        console.log('soy asincrona ');
+    },1000);
+
+}
+console.log('iniciando proceso');
+soyAsincrona();
+console.log('terminando proceso');
